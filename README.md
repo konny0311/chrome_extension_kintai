3b4732490d3e495e9cea97dd2d4fcc9372b9dc40
konny0311 <tatsuya.kono.dev@gmail.com>
2022-02-11 21:07:38 +0900

    get ave working hours

---
+++ README.md
@@ -0,0 +1,2 @@
+# Extension for Kintai
+This extension creates a line chart using data of 勤怠項目別集計 for managers.
--- board.js
+++ board.js
@@ -6,21 +6,46 @@ debugButton.addEventListener("click", async () => {
     // "scripting" permission required in manifest.json
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        function: getMembers,
+        function: getInfo
     });
 });
 
-function getMembers() {
+function getInfo() {
+    // get members name
     let memberTable = document.getElementsByClassName("attendance-table-contents")[1];
-    console.log(memberTable);
+    let members = [];
     let numRows = memberTable.rows.length;
     console.log(numRows);
     for (i = 0; i < numRows; i++) {
         var cells = memberTable.rows.item(i).cells;
         var nameCell = cells[0];
-        console.log(nameCell);
         let name = nameCell.getElementsByClassName("attendance-custom-input-text fs-exclude")[0].innerHTML;
         console.log(name);
+        members.push(name);
     }
-    console.log("aaaaa");
+    console.log(members);
+
+    // get working hours
+    let aveHours = [];
+    let attendanceTable = document.getElementsByClassName("attendance-table-contents")[3];
+    console.log(attendanceTable);
+    let attendanceRows = attendanceTable.rows.length;
+    for (i = 0; i < attendanceRows; i++) {
+        let cells = attendanceTable.rows.item(i).cells;
+        let daysCell = cells[0];
+        let hoursCell = cells[6];
+        let days = parseFloat(daysCell.innerHTML.replace(/[\u3400-\u9FFF]/g, "")); // Remove Kanji
+        let hours = parseFloat(hoursCell.innerHTML.replace("h", "")); // Remove h
+        if (days != 0) {
+            let ave = hours/days;
+            aveHours.push(ave);
+            console.log("Average working hours per day", ave);
+        } else {
+            aveHours.push(0);
+            console.log("No working day so far.")
+        }
+    }
+
+    console.log(members.length);
+    console.log(aveHours.length);
 }
