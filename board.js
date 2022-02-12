81446e584e1be1eb912ccbad4fd8887c05fed930
konny0311 <tatsuya.kono.dev@gmail.com>
2022-02-12 14:37:18 +0900

    Descending sort the result

--- board.js
+++ board.js
@@ -4,38 +4,25 @@ debugButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active:true, currentWindow: true});
 
     // "scripting" permission required in manifest.json
-    var result = chrome.scripting.executeScript({
+    var ret = chrome.scripting.executeScript({
         target: {tabId: tab.id},
         function: getInfo
     });
-    // console.log(result);
-    result.then(val => {insertInfo(val[0]["result"])})
+    ret.then(val => {insertInfo(val[0]["result"])})
           .catch(err => {console.log(err)});
 });
-
-function onExecuted(result) {
-    console.log(`Execution success`);
-    console.log(result);
-  }
-  
-function onError(error) {
-console.log(`Error: ${error}`);
-}
 
 function getInfo() {
     // get members name
     let memberTable = document.getElementsByClassName("attendance-table-contents")[1];
     let members = [];
     let numRows = memberTable.rows.length;
-    console.log(numRows);
     for (i = 0; i < numRows; i++) {
         var cells = memberTable.rows.item(i).cells;
         var nameCell = cells[0];
         let name = nameCell.getElementsByClassName("attendance-custom-input-text fs-exclude")[0].innerHTML;
-        console.log(name);
         members.push(name);
     }
-    console.log(members);
 
     // get working hours
     let aveHours = [];
@@ -50,32 +37,35 @@ function getInfo() {
         if (days != 0) {
             let ave = hours/days;
             aveHours.push(ave);
-            console.log("Average working hours per day", ave);
         } else {
             aveHours.push(0);
-            console.log("No working day so far.")
         }
     }
 
-    console.log(members.length);
-    console.log(aveHours.length);
-
-    return [members, aveHours];
+    var info = {};
+    members.map(function(member, i) {
+         info[member] = aveHours[i]
+        });
+    
+    // Descending sort
+    var sortedInfo = Object.keys(info).map((k)=>({ key: k, value: info[k] }));
+    sortedInfo.sort((a, b) => a.value - b.value);
+    return sortedInfo;
 }
 
-function insertInfo(result) {
+function insertInfo(info) {
     let tbody = document.getElementById("attendanceBody");
-    let num = result[0].length;
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < info.length; i++) {
         let newRow = tbody.insertRow(0);
         var newCell = newRow.insertCell(0);
-        var newText = document.createTextNode(result[1][i]);
+        // Hours
+        var newText = document.createTextNode(info[i].value);
         newCell.appendChild(newText);
         var newCell = newRow.insertCell(0);
-        var newText = document.createTextNode(result[0][i]);
+        // Name
+        var newText = document.createTextNode(info[i].key);
         newCell.appendChild(newText);
-    }
-    // TODO: Receive members and aveHours variables from getInfo()    
+    };
 }
 
 
