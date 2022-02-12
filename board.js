1ac6e47e290a544429cef39a73f54631e1f35117
konny0311 <tatsuya.kono.dev@gmail.com>
2022-02-12 10:02:20 +0900

    insert info into extension popup

--- board.js
+++ board.js
@@ -4,11 +4,23 @@ debugButton.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active:true, currentWindow: true});
 
     // "scripting" permission required in manifest.json
-    chrome.scripting.executeScript({
+    var result = chrome.scripting.executeScript({
         target: {tabId: tab.id},
         function: getInfo
     });
+    // console.log(result);
+    result.then(val => {insertInfo(val[0]["result"])})
+          .catch(err => {console.log(err)});
 });
+
+function onExecuted(result) {
+    console.log(`Execution success`);
+    console.log(result);
+  }
+  
+function onError(error) {
+console.log(`Error: ${error}`);
+}
 
 function getInfo() {
     // get members name
@@ -28,7 +40,6 @@ function getInfo() {
     // get working hours
     let aveHours = [];
     let attendanceTable = document.getElementsByClassName("attendance-table-contents")[3];
-    console.log(attendanceTable);
     let attendanceRows = attendanceTable.rows.length;
     for (i = 0; i < attendanceRows; i++) {
         let cells = attendanceTable.rows.item(i).cells;
@@ -48,4 +59,23 @@ function getInfo() {
 
     console.log(members.length);
     console.log(aveHours.length);
+
+    return [members, aveHours];
 }
+
+function insertInfo(result) {
+    let tbody = document.getElementById("attendanceBody");
+    let num = result[0].length;
+    for (i = 0; i < num; i++) {
+        let newRow = tbody.insertRow(0);
+        var newCell = newRow.insertCell(0);
+        var newText = document.createTextNode(result[1][i]);
+        newCell.appendChild(newText);
+        var newCell = newRow.insertCell(0);
+        var newText = document.createTextNode(result[0][i]);
+        newCell.appendChild(newText);
+    }
+    // TODO: Receive members and aveHours variables from getInfo()    
+}
+
+
